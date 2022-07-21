d8326e460dce9ffdcdf3927ea097f06d673075f1
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-07-21 11:44:48 +0900

    406
    interface finish

---
+++ src/sec02/exam10/InterfaceB.java
@@ -0,0 +1,5 @@
+package sec02.exam10;
+
+public interface InterfaceB {
+    public void methodB();
+}
---
+++ src/sec02/exam10/InterfaceC.java
@@ -0,0 +1,5 @@
+package sec02.exam10;
+
+public interface InterfaceC extends InterfaceA, InterfaceB{
+    public void methodC();
+}
